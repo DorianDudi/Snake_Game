a63df78b4c27146bebf6fdaba52fa4679009bf99
Dorian-DV <ddorianvlad@yahoo.ro>
2022-11-20 14:38:44 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,57 +7,54 @@ let gameCanvas = document.getElementById('game');
 ctx.fillStyle = 'green';
 ctx.fillRect(x, y, 10, 10);
 
-
 function moveRight() {
 	x += 10;
-    ctx.clearRect(0, 0, 600, 600);
-    ctx.fillRect(x, y, 10, 10);
-    //console.log("movingRight");
+	ctx.clearRect(0, 0, 600, 600);
+	ctx.fillRect(x, y, 10, 10);
 }
 
 function moveLeft() {
 	x -= 10;
-    ctx.clearRect(0, 0, 600, 600);
-    ctx.fillRect(x, y, 10, 10);
+	ctx.clearRect(0, 0, 600, 600);
+	ctx.fillRect(x, y, 10, 10);
 }
 
 function moveUp() {
 	y -= 10;
-    ctx.clearRect(0, 0, 600, 600);
-    ctx.fillRect(x, y, 10, 10);
+	ctx.clearRect(0, 0, 600, 600);
+	ctx.fillRect(x, y, 10, 10);
 }
 
 function moveDown() {
 	y += 10;
-    ctx.clearRect(0, 0, 600, 600);
-    ctx.fillRect(x, y, 10, 10);
+	ctx.clearRect(0, 0, 600, 600);
+	ctx.fillRect(x, y, 10, 10);
 }
 
 function move(Dir) {
-	if	(Dir == 1) {
-    	moveRight();
-    } else if (Dir == 2) {
-    	moveDown();
-    } else if (Dir == 3) {
-    	moveLeft();
-    } else {
-    	moveUp();
-    }
+	if (Dir == 1) {
+		moveRight();
+	} else if (Dir == 2) {
+		moveDown();
+	} else if (Dir == 3) {
+		moveLeft();
+	} else {
+		moveUp();
+	}
 }
 
-function changeDirection(event) {	// key listeners
+function changeDirection(event) {// key listeners
 	if (event.key === 'ArrowRight') {
-    	currentDirection = 1;
-    } else if (event.key === 'ArrowDown') {
-    	currentDirection = 2;
-    } else if (event.key === 'ArrowLeft') {
-    	currentDirection = 3;
-    } else if (event.key === 'ArrowUp') {
-    	currentDirection = 4;
+		currentDirection = 1;
+	} else if (event.key === 'ArrowDown') {
+		currentDirection = 2;
+	} else if (event.key === 'ArrowLeft') {
+		currentDirection = 3;
+	} else if (event.key === 'ArrowUp') {
+		currentDirection = 4;
 	}
-    //console.log(currentDirection);
 }
-//document.addEventListener('keydown', move);
+
 function passMoveParam() {
 	move(currentDirection);
 }
@@ -67,8 +64,8 @@ document.addEventListener('keydown', changeDirection);
 
 
 function delayedMove() {
-    setTimeout(move, 500 * countSteps, currentDirection);
-    ++countSteps;
+	setTimeout(move, 500 * countSteps, currentDirection);
+	++countSteps;
 }
 
 while (countSteps < 20) {
@@ -78,14 +75,14 @@ while (countSteps < 20) {
 /*
 while (countSteps < 20) {
 	delayedMove();
-    if (x > 590 || x < 0 || y > 590 || y < 0) {
+	if (x > 590 || x < 0 || y > 590 || y < 0) {
 		gameOver = 1;
-        alert('Game Over');
-    }
+		alert('Game Over');
+	}
 
-    if (countSteps < 10) {
+	if (countSteps < 10) {
 		gameOver = 1;
-        alert('Game Over');
-    }
+	alert('Game Over');
+	}
 }
 */
